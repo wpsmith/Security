4e02065186faec62dbceaf75fd66a30e941b7ca3
wpsmith <t@wpsmith.net>
2019-08-26 19:08:36 -0400

    Load muplugin plugins

--- src/Security.php
+++ src/Security.php
@@ -53,12 +53,12 @@ if ( ! class_exists( '\WPS\WP\Security' ) ) {
 				Admin\HeartbeatThrottle::get_instance();
 			}
 
-			if ( $this->args['force_strong_passwords'] && file_exists( __DIR__ . '/force-strong-passwords/slt-force-strong-passwords.php' ) ) {
-				require __DIR__ . '/force-strong-passwords/slt-force-strong-passwords.php';
+			if ( $this->args['force_strong_passwords'] && file_exists( ABSPATH . MUPLUGINDIR . '/force-strong-passwords/slt-force-strong-passwords.php' ) ) {
+				require ABSPATH . MUPLUGINDIR . '/force-strong-passwords/slt-force-strong-passwords.php';
 			}
 
-			if ( $this->args['limit_login_attempts'] && file_exists( __DIR__ . '/limit-login-attempts-reloaded/limit-login-attempts-reloaded.php' ) ) {
-				require __DIR__ . '/limit-login-attempts-reloaded/limit-login-attempts-reloaded.php';
+			if ( $this->args['limit_login_attempts'] && file_exists( ABSPATH . MUPLUGINDIR . '/limit-login-attempts-reloaded/limit-login-attempts-reloaded.php' ) ) {
+				require ABSPATH . MUPLUGINDIR . '/limit-login-attempts-reloaded/limit-login-attempts-reloaded.php';
 			}
 
 			if ( $this->args['disable_auto_update'] ) {
